94d6d5f913ebaf5683632dd4af09e39289794aaf
Derek Podimatis <derek@Dereks-MacBook-Pro.local>
2022-06-24 22:21:28 -0700

    Style button right, add lorem for pages

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,9 +2,9 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
 import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
-import TwitterProvider from "next-auth/providers/twitter"
-import Auth0Provider from "next-auth/providers/auth0"
+//import FacebookProvider from "next-auth/providers/facebook"
+//import TwitterProvider from "next-auth/providers/twitter"
+//import Auth0Provider from "next-auth/providers/auth0"
 import EmailProvider from "next-auth/providers/email"
 import AppleProvider from "next-auth/providers/apple";
 
--- pages/login-test/b.js
+++ pages/login-test/b.js
@@ -13,12 +13,13 @@ export default function LoginTest() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className={styles.center}>
+      <header className={styles.right}>
         <LoginBtn />
       </header>
     
-      <div>
-        <p> Page B</p>
+      <div className='center'>
+        <h1> Page B</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <br />
         <Link href="/"><a >Return to Index</a></Link>
       </div>
